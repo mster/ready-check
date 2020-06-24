eca8f5f1ec81c33b3c2d257e76bfb31bfbe3ef67
mster <michaelsterpka@gmail.com>
2020-06-23 19:02:55 -0700

    add docker & lint with standard js

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 const Discord = require('discord.js')
-const client = new Discord.Client()
+const debug = require('util').debuglog('ready-check')
 
+const client = new Discord.Client()
 const config = require('./config.json')
+
+const STATE = {
+  checking: false
+}
 
 client.on('ready', () => {
   // Bot is ready
-  console.log(`Bot has started.`)
-  client.user.setActivity(`you.`, { type: 'WATCHING' })
+  debug('Bot has started.')
+  client.user.setActivity('you.', { type: 'WATCHING' })
 })
-
-let checking = false
 
 client.on('message', async message => {
   // good house keeping
@@ -27,35 +30,49 @@ client.on('message', async message => {
   let designation = '@here'
   if (args[0] && args[0].includes('@')) designation = args.shift()
 
+  // help command prints usage
+  if (command === 'help') {
+    message.channel.send(
+      `<@${message.author.id}>, to run a ready check use the command:\n\`rdy check [@designation] topic\`.\n\nFor example, \`rdy check @everyone Summer Olypmics\``
+    )
+    message.delete().catch(e => {})
+
+    return
+  }
+
   // rdy check [@designation] topic -> '@designation Are we ready for topic?'
   if (command === 'check') {
-    if (checking) {
+    if (STATE.checking) {
       message.delete().catch(e => {})
+
       const reply = await message.channel.send(
         `<@${message.author.id}>, We are already running a ready check.`
       )
+
+      // delete the reply after 10 seconds
       setTimeout(() => {
         reply.delete().catch(e => {})
-      }, 5000)
+      }, 10000)
 
       return
     }
 
-    checking = true
+    STATE.checking = true
 
     const msg = await message.channel.send(
       `${designation} Are we ready for ${args.join(' ')}?`
     )
-    // add template reactions
+
+    // add template reactions for users to click
     try {
       await msg.react('❓')
       await msg.react('👍')
       await msg.react('👎')
-    } catch (err) {
-      onError(err)
+    } catch (error) {
+      errorOut(error)
     }
 
-    // perform collection
+    // perform collection of reactions
     // https://discordjs.guide/popular-topics/reactions.html
     const filter = (reaction, user) => {
       return (
@@ -68,52 +85,52 @@ client.on('message', async message => {
     }
     const collector = await msg.createReactionCollector(filter, collectorOpts)
 
-    // add listeners
-    collector.on('collect', onCollect)
-    collector.on('end', onEnd)
-    collector.on('error', onError)
+    // add listeners for each event
+    collector.on('collect', collected => {
+      debug('ON-COLLECT')
+    })
 
-    // listeners
-    function onCollect (reaction) {
-      console.log('ON-COLLECT')
-    }
-
-    function onEnd (collected) {
-      console.log('ON-END')
+    collector.on('end', collected => {
+      debug('ON-END')
       try {
         const hasYea = collected.has('👍')
         const hasNay = collected.has('👎')
 
-        if (hasNay)
+        if (hasNay) {
           message.channel.send(
             `${designation} We are NOT ready for ${args.join(' ')}!`
           )
-        else if (!hasNay && hasYea)
+        } else if (!hasNay && hasYea) {
           message.channel.send(
             `${designation} We are ready for ${args.join(' ')}!`
           )
-        else
+        } else {
           message.channel.send(
             `${designation} Nobody responded to my ready check...`
           )
+        }
 
         msg.delete().catch(e => {})
-      } catch (err) {
-        onError(err)
+      } catch (error) {
+        errorOut(error)
       }
 
-      checking = false
-    }
+      // revert state after our check has completed
+      STATE.checking = false
+      collector.removeAllListeners()
+    })
 
-    function onError (err) {
-      msg.channel.send(
-        'Uh oh! Something went *fucky-wucky* with our check. Go ping the author with this error message'
-      )
-      msg.channel.send(err)
-
-      process.exitCode = 1
-    }
+    collector.on('error', error => {
+      msg.channel.send('Uh oh! Something went *funky* with our check.')
+      errorOut(error)
+    })
   }
 })
+
+function errorOut (error) {
+  // just console.error with exitCode setting
+  process.exitCode = 1
+  console.error(error)
+}
 
 client.login(config.token)
